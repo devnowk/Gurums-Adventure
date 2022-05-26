13488f3481a1892ddfe5878ef7d421a9cfd85db5
devnowk <57873373+devnowk@users.noreply.github.com>
2022-05-27 01:48:13 +0900

    메인씬
    1. gameManager 마지막 단계에 active 될 오브젝트 설정

--- Gurums-Adventure2/Assets/Scripts/gameManager.cs
+++ Gurums-Adventure2/Assets/Scripts/gameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class gameManager : MonoBehaviour
 {
     public GameObject[] door = new GameObject[3]; // 클리어 해야 하는 미니 게임 문 받아옴
+    public GameObject[] level_3_object; // 3단계에 active해야 하는 오브젝트
     public int level = 1; // 현재 클리어해야 하는 단계
 
     public static gameManager I; // 싱글톤
@@ -15,17 +16,25 @@ public class gameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("Twinkle", 0f, 0.5f); // 씬이 로드되면 Twinkle 함수를 0.5초마다 반복
+        if(level==3)
+        {
+            for(int i=0; i<level_3_object.Length; i++)
+                level_3_object[i].gameObject.SetActive(true);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        Twinkle(); // MainScene에 있으면 문 반짝반짝 빛나게 하기
+        
     }
 
-    void Twinkle()
+    void Twinkle() // 문 반짝이기
     {
-
+        // Mathf.PingPong : 시간이 흐를수록 값을 증감시키는 함수
+        // Color.Lerp : 색깔을 서서히 변경해줌
+        door[level - 1].GetComponent<Renderer>().material.color = Color.Lerp(Color.gray, Color.yellow, Mathf.PingPong(Time.time, 1));
+        door[level - 1].GetComponent<Renderer>().material.color = Color.Lerp(Color.yellow, Color.gray, Mathf.PingPong(Time.time, 1));
     }
 }
