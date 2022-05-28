d17a08ba6e824309582850070972ec227eb807c1
devnowk <57873373+devnowk@users.noreply.github.com>
2022-05-29 00:41:45 +0900

    DoorOpen 회전문
    플레이어가 문 앞에 있으면 회전문이 열림

--- Gurums-Adventure2/Assets/Scripts/DoorOpen.cs
+++ Gurums-Adventure2/Assets/Scripts/DoorOpen.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class DoorOpen : MonoBehaviour
 {
+    public int doorType = 0; // 0:회전문, 1:자동문
+    public float maxAngle = 96; // 최대 열리는 각도(회전문 96)
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,17 @@ public class DoorOpen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" && doorType == 0) // 회전문 앞에 서면 문 열림
+            PushDoor();
+    }
+    void PushDoor()
+    {
+        if(transform.localEulerAngles.y < maxAngle)
+            transform.rotation *= Quaternion.AngleAxis(0.5f, Vector3.up); // y축 방향으로 회전
+        //Debug.Log(transform.localEulerAngles.y);
     }
 }
